18134a802cb1a1094ce02751033f84309aa89a3a
Agustin Pianciola <85904367+agustinpianciola@users.noreply.github.com>
2022-10-20 18:06:10 -0300

    Pricelist code

---
+++ pricelist_code/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Pricelist based on cost',
+    'summary': "Pricelist bassed n costAdd option to give global dicount on sale order and Invoice by fix amount or by percentage",
+    'description': "Pricelist bassed n cost",
+
+    'author': 'ZOLVANT',
+    'website': 'zolvant.com',
+    'support': 'zolvant.com',
+
+    'category': 'Sales',
+    'version': '15.0.0.1.0',
+    'depends': ['product'],
+
+    'data': [],
+
+    'license': "OPL-1",
+    
+
+    'auto_install': False,
+    'installable': True,
+
+    'images': [],
+   
+}
